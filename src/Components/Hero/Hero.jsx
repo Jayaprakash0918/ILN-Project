7b050e026a4b5d25f71bc58c17484915a63a9bd1
sheelam jayaprakash <sheelamjayaprakash@gmail.com>
2023-12-02 23:07:30 +0530

    Added blur background to navbar

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
     <div className='hero'>
         <div className='hero-left-container'>
           <div className='title'>
-          title
+          Build and Create Dream
           </div>
           <div className='para'>
           Together with Innovate Learn Nexus
